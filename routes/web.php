85551cc6b73c81014c8656b1abfa25160058b632
saraiGtz <sgutierrez@videsa.com.mx>
2025-06-09 13:24:12 -0600

    update

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PdfController;
 use App\Http\Controllers\ServiceOrdersController;
 use App\Http\Controllers\UsersController;
-use App\Http\Controllers\nom085_Controller;
 
 use App\Http\Controllers\EmpresaController;
 
@@ -46,8 +45,8 @@ Route::post('/register_user', [UsersController::class, 'RegistrarUsuarios'])->na
 Route::get('/getMuestreadores', [FormsController::class, 'getMuestreadores']);
 
 Route::post('/generarPDF', [PdfController::class, 'generarPDF'])->name('generarPDF');
-Route::get('/prueba_puntos', [Nom, 'distribucion_puntos_estratificacion'])->name('Prueba');
+Route::get('/prueba_puntos', [Nom_085_Campo_Controller::class, 'distribucion_puntos_estratificacion'])->name('Prueba');
 
-Route::post('/registrar_nom85mg', [nom085_Controller::class, 'registrar_nom085_mg'])->name('registrar_nom085_mg');
+
 Route::get('/empresa/nueva', [EmpresaController::class, 'create'])->name('empresa.create');
 Route::post('/empresa/guardar', [EmpresaController::class, 'store'])->name('empresa.store');
