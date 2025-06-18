e41bf4b07b453b1b5906af567793f4b4cfa996b6
DavidEM263 <despinoza@videsa.com.mx>
2025-06-18 17:03:30 -0600

    controlador api login

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,18 @@
 <?php
 
-use App\Http\Controllers\Api\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\LoginController;
 use App\Http\Controllers\Api\ServicioController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/login', [LoginController::class, 'IniciarSesion'])->name('login_api');
 
-Route::post('/login', [LoginController::class, 'login_api'])->name('login_api');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
 
+    Route::get('/servicios', [ServicioController::class, 'obtenerServicios']);
 
-
-
-
-Route::get('/servicios', [ServicioController::class, 'obtenerServicios']);
+    Route::post('/logout', [LoginController::class, 'LogOut'])->name('logout_api');
+});
